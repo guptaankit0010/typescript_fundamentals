f2dd1b035a4727743b694091676715ebf1da022c
Ankit Gupta <ankit.gupta1@radisys.com>
2025-02-11 22:43:03 +0530

    ts optional params

--- app.ts
+++ app.ts
@@ -1,1 +1,26 @@
-console.log("hello");
+function GetRating(title: string): string | number {
+    if (title == 'A new hope')
+        return 'Calssic';
+    return Math.round((Math.random() * 10))
+}
+
+function PrintMovieInfo(title: string, releaseYear?: Number, ...cast: string[]) {
+    console.log(`Title = ${title}`);
+
+    if (releaseYear) {
+        console.log(`Release year = ${releaseYear}`)
+    }
+
+    for (const name of cast) {
+        console.log(`cast : ${name}`)
+    }
+}
+
+PrintMovieInfo("movie 1", 1976);
+const castMembers = ["hen", "cat"];
+PrintMovieInfo("movie 2", 1987, ...castMembers);
+
+let movieName: string = "A new hopsse";
+
+let movieRating: string | number = GetRating(movieName);
+console.log(movieRating)
--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,35 @@
-console.log("hello");
+"use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+function GetRating(title) {
+    if (title == 'A new hope')
+        return 'Calssic';
+    return Math.round((Math.random() * 10));
+}
+function PrintMovieInfo(title, releaseYear) {
+    var cast = [];
+    for (var _i = 2; _i < arguments.length; _i++) {
+        cast[_i - 2] = arguments[_i];
+    }
+    console.log("Title = ".concat(title));
+    if (releaseYear) {
+        console.log("Release year = ".concat(releaseYear));
+    }
+    for (var _a = 0, cast_1 = cast; _a < cast_1.length; _a++) {
+        var name_1 = cast_1[_a];
+        console.log("cast : ".concat(name_1));
+    }
+}
+PrintMovieInfo("movie 1", 1976);
+var castMembers = ["hen", "cat"];
+PrintMovieInfo.apply(void 0, __spreadArray(["movie 2", 1987], castMembers, false));
+var movieName = "A new hopsse";
+var movieRating = GetRating(movieName);
+console.log(movieRating);
